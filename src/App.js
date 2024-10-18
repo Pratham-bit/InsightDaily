bb6e6d1a07bf67aef9c5a354b342a218e83e49d0
Pratham-bit <85451959+Pratham-bit@users.noreply.github.com>
2024-10-18 12:25:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoadingBar from 'react-top-loading-bar';
 
 export default class App extends Component {
-  apiKey = process.env.REACT_APP_NEWS_API
   state = {
     progress: 0,
   }
